360987447c86ccdb8266fc63f1f764b031d37b86
Amin Talebi <talebi242@gmail.com>
2017-11-02 01:59:28 +0330

    search and auto complete

--- zarpolbo/cafe/urls.py
+++ zarpolbo/cafe/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'cafe'
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^search/', views.search, name='search')
 ]
--- zarpolbo/cafe/views.py
+++ zarpolbo/cafe/views.py
@@ -1,4 +1,8 @@
+import json
+
+from django.core import serializers
 from django.http import Http404
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -14,10 +18,12 @@ def index(request):
     except:
         raise Http404("No Cafe")
     context = {
-        'cafes_list' : cafes_list
+        'cafes_list': cafes_list
     }
     return render(request, 'cafe/index.html', context)
 
 
 def search(request):
-    pass
+    print(request.GET.get('q'))
+    cafe_list = Cafe.objects.filter(name__contains=request.GET.get('q'))
+    return HttpResponse(serializers.serialize('json', cafe_list), content_type="application/json")

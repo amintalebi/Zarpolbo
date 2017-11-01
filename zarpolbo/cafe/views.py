ff9a387e008c9dcb96e15b8029f8c4951743d01d
Amin Talebi <talebi242@gmail.com>
2017-11-01 15:05:12 +0330

    context

--- zarpolbo/cafe/views.py
+++ zarpolbo/cafe/views.py
@@ -1,12 +1,22 @@
+from django.http import Http404
 from django.shortcuts import render
 
 # Create your views here.
 from django.template import loader
 from django.views.generic.edit import FormView
 
+from cafe.models import Cafe
+
 
 def index(request):
-    return render(request, 'cafe/index.html')
+    try:
+        cafes_list = Cafe.objects.all()
+    except:
+        raise Http404("No Cafe")
+    context = {
+        'cafes_list' : cafes_list
+    }
+    return render(request, 'cafe/index.html', context)
 
 
 

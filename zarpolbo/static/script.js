cae45aa21cdeea64e6dc1a51bcf341418ba26f33
Kiarash Golezardi <golezardi@ce.sharif.edu>
2017-11-02 10:48:14 +0330

    Show map on search

--- zarpolbo/static/script.js
+++ zarpolbo/static/script.js
@@ -24,5 +24,50 @@ function search() {
                 `);
         }
 
+        $("#mapp").empty();
+        loadMap(data);
+
     }, "json");
+}
+
+function loadMap(data)
+{
+    var latSum = 0, lngSum = 0, cnt = 0;
+
+    for (cafe of data)
+    {
+        latSum += cafe.fields.latitude;
+        lngSum += cafe.fields.longitude;
+        cnt++;
+    }
+
+    var locations = [];
+    for (cafe of data)
+        locations.push([cafe.fields.name, cafe.fields.latitude, cafe.fields.longitude]);
+
+    alert(locations);
+
+    var map = new google.maps.Map(document.getElementById('mapp'), {
+        zoom: 10,
+        center: new google.maps.LatLng(latSum / cnt, lngSum / cnt),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: map
+        });
+
+        google.maps.event.addListener(marker, 'click', (function (marker, i) {
+            return function () {
+                infowindow.setContent(locations[i][0]);
+                infowindow.open(map, marker);
+            }
+        })(marker, i));
+    }
 }

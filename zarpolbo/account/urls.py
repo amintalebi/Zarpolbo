032ffda94ef132a425a2e194b1319a8c571c7635
Kiarash Golezardi <golezardi@ce.sharif.edu>
2017-11-02 10:02:12 +0330

    Add login page

--- zarpolbo/account/urls.py
+++ zarpolbo/account/urls.py
@@ -1,8 +1,9 @@
 # coding=utf-8
 from django.conf.urls import url
-
-from views import RegisterView
+from . import views
 
 urlpatterns = [
-    url(r'^register/$', RegisterView.as_view(), name='register')
+    url(r'^register/$', views.register_view, name='register'),
+    url(r'^logout/$', views.logout_view, name='logout'),
+    url(r'^login/$', views.LoginView.as_view(), name='login')
 ]
--- zarpolbo/account/views.py
+++ zarpolbo/account/views.py
@@ -1,27 +1,55 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.views.generic import FormView
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
 
 from account.forms import RegisterForm
+from django.contrib.auth.views import login as django_login_view
 
 
-class RegisterView(FormView):
+def register_view(request):
+    # Should not be logged in
+    if request.user.is_authenticated:
+        return redirect('/')
+
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            name = form.cleaned_data.get('name')
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password1')
+
+            if User.objects.filter(username=username).count() != 0:
+                form.add_error('username', 'A user with this username already exists.')
+            else:
+                User.objects.create_user(username=username, email=email, password=password, first_name=name)
+                return redirect('/account/login/')
+    else:
+        form = RegisterForm()
+    return render(request, 'account/register.html', {'form': form})
+
+
+class LoginView(FormView):
+    success_url = '/'
+    form_class = AuthenticationForm
     template_name = 'account/register.html'
-    form_class = RegisterForm
-    success_url = '/account/login/'
 
     def form_valid(self, form):
-        result = super(RegisterView, self).form_valid(form)
         username = form.cleaned_data.get('username')
-        name = form.cleaned_data.get('name')
-        email = form.cleaned_data.get('email')
-        raw_password = form.cleaned_data.get('password1')
-        if User.objects.filter(username=username).count() == 0:
-            User.objects.create_user(username=username, email=email, password=raw_password, first_name=name)
+        password = form.cleaned_data.get('password')
+        user = authenticate(self.request, username=username, password=password)
+        if user is not None:
+            login(self.request, user)
 
-        return result
+        return super(LoginView, self).form_valid(form)
 
 
+@login_required(login_url='/account/login/')
+def logout_view(request):
+    logout(request)
+    return redirect('/')
